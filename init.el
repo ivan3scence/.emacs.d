fc7b5b16fafb15e1473e606a211a4d5e89f015a3
Gorichenko Ivan <i.gorichenko@rt-solar.ru>
2024-08-21 18:24:42 +0300

    init

---
+++ init.el
@@ -0,0 +1,28 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons-dired auto-complete-clang bash-completion company-go
+                         counsel cpputils-cmake dashboard dired-open
+                         doom-modeline doom-themes emojify
+                         eshell-syntax-highlighting evil-collection
+                         evil-tutor gcmh general gruber-darker-theme
+                         ivy-posframe ivy-rich magit neotree
+                         peep-dired perspective projectile smex
+                         sudo-edit swiper toc-org vterm which-key
+                         writeroom-mode))
+ '(persp-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
